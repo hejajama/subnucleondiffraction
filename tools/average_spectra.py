6d0e708f4fb62c2599d119bd27c3f16132776941
Heikki Mäntysaari <mantysaari@bnl.gov>
2015-10-26 14:00:30 -0400

    Average tool, missing parameter file

---
+++ tools/average_spectra.py
@@ -0,0 +1,51 @@
+# utf-8-
+
+# Average the calculated spectra
+
+import os
+import sys
+
+sys.path.append("/Users/heikki/lib/")
+from matplotlibhelper import *
+
+dir="../testit/"
+
+tdata=[]
+ydata=[]
+
+first=1
+
+tmpxdatas=[]
+tmpydatas=[]
+
+for f in os.listdir(dir):
+    tmpydata=[]
+    tmpxdata=[]
+    
+    fname = dir + f
+
+    readfile_xy(fname, tmpxdata, tmpydata)
+
+    tmpxdatas.append(tmpxdata)
+    tmpydatas.append(tmpydata)
+
+# average
+nconf = len(tmpydatas)
+
+print "# Number of files: " + str(len(tmpydatas))
+
+for i in range(len(tmpydatas[0])):
+    t = tmpxdatas[0][i]
+    try:    # if some file is not calcualted to high enough t, this we just skip that t value
+        sum=0
+        for j in range(len(tmpydatas)):
+            sum+=tmpydatas[j][i]
+        avg = sum/len(tmpydatas)
+
+        tdata.append(t)
+        ydata.append(avg)
+    except:
+        print "# Skipping t index " + str(i) + " t=" + str(t)
+
+for x,y in zip(tdata, ydata):
+    print x,y
