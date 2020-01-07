ad524a80e5ea774c8a6a19a2cc34c1eff8350111
Heikki Mäntysaari <heikki.mantysaari@jyu.fi>
2020-01-07 08:18:40 +0200

    Support real and imag parts, tune integration limits

--- tools/baryon.cpp
+++ tools/baryon.cpp
@@ -18,26 +18,21 @@ gsl_rng* global_rng;
 using namespace std;
 
 
-const double L = 7*Amplitude::FMGEV;
+double L = 7*Amplitude::FMGEV;
 const double MCINTACCURACY=0.1;
 
 struct inthelper
 {
     IPGlasma *glasma;
+    bool imaginary_part;
 };
 
 // Baryon profile
 double T_baryon(Vec v1, Vec v2, Vec v3)
 {
     const double B=4;
-    Vec center = v1+v2;
-    center = center + v3;
-    center = center*0.333;
-    v1 = v1 - center;
-    v2 = v2 - center;
-    v3 = v3 - center;
     
-    return std::exp(-(v1.LenSqr() + v2.LenSqr() + v3.LenSqr())/(2.0*B));
+    return 1.0/std::pow(2.0*M_PI*B, 3) * std::exp(-(v1.LenSqr() + v2.LenSqr() + v3.LenSqr())/(2.0*B));
 }
 
 // MC vector: (bx,by,q1x,q1y,q2x,q2y,q3x,q3y)
@@ -48,6 +43,9 @@ double inthelperf_mc(double* vec, size_t dim, void* p)
     Vec q1(vec[2],vec[3]);
     Vec q2(vec[4],vec[5]);
     Vec q3(vec[6],vec[7]);
+    
+    double density = T_baryon(q1,q2,q3);
+    
     Vec b(vec[0],vec[1]);
     // b independent target -> more statistics
     q1 = q1 + b;
@@ -60,7 +58,10 @@ double inthelperf_mc(double* vec, size_t dim, void* p)
     
     complex<double> baryon = par->glasma->BaryonOperator(0.01, q1v,q2v,q3v);
     
-    return baryon.real() * T_baryon(q1,q2,q3) / std::pow(L, 8);
+    if (par->imaginary_part == false)
+        return baryon.real() * density / std::pow(L, 2);
+    else
+        return baryon.imag() * density / std::pow(L, 2);
 }
 
 
@@ -70,8 +71,9 @@ int main(int argc, char* argv[])
 {
     // Arguments: ipglasma filename  b  schwinger_r
     string fname = argv[1];
-    int mcintpoints = 5e7;
-    cout << "# Filename: " << fname << " points " << mcintpoints  << endl;
+    int mcintpoints = StrToReal(argv[2]);
+    L = StrToReal(argv[3])*Amplitude::FMGEV;
+    cout << "# Filename: " << fname << " points " << mcintpoints  << " L = " << L/Amplitude::FMGEV << " fm" << endl;
 
     
     gsl_rng_env_setup();
@@ -94,7 +96,12 @@ int main(int argc, char* argv[])
     double max[8] = {L/2.0, L/2.0, L/2.0, L/2.0, L/2.0,L/2.0,L/2.0,L/2.0 };
     
     gsl_monte_miser_state *s = gsl_monte_miser_alloc (dim);
-    double result,abserr; int iter=0;
+    double result_real=0; double result_imag=0;
+    
+    
+    /// Real part
+    helper.imaginary_part=false;
+    double abserr; int iter=0;
     do
     {
         iter++;
@@ -105,28 +112,36 @@ int main(int argc, char* argv[])
         }
         //gsl_monte_plain_integrate
         gsl_monte_miser_integrate
-            (&fun, min, max, 8, mcintpoints, global_rng, s,
-                               &result, &abserr);
-        cout << "# iter " << iter << " res " << result << " +/- " << abserr <<endl;
+            (&fun, min, max, dim, mcintpoints, global_rng, s,
+                               &result_real, &abserr);
+        cout << "# real part iter " << iter << " res " << result_real << " +/- " << abserr <<endl;
             //if (std::abs(abserr/result)>0.2)
                   //cerr << "#r=" << r << " misermc integral failed, result " << result << " relerr " << std::abs(abserr/result) << ", again.... (iter " << iter << ")" << endl;
-    } while (std::abs(abserr/result)>MCINTACCURACY);
-    cout << result << " " << abserr << endl;
+    } while (std::abs(abserr/result_real)>MCINTACCURACY);
+    
+    //// Imaginary
+    iter=0;
+    helper.imaginary_part=true;
+    do
+    {
+        iter++;
+        if (iter>=4)
+        {
+            cerr << "Imaginary part mcintegral didn't converge, result " << result_imag << " +/- " << abserr << endl;
+            break;
+        }
+        //gsl_monte_plain_integrate
+        gsl_monte_miser_integrate
+            (&fun, min, max, dim, mcintpoints, global_rng, s,
+                               &result_imag, &abserr);
+        cout << "# imaginary part iter " << iter << " res " << result_imag << " +/- " << abserr <<endl;
+          
+    } while (std::abs(abserr/result_imag)>MCINTACCURACY);
+    cout << result_real  << " " << result_imag << endl;
     //gsl_monte_plain_free (s);
     gsl_monte_miser_free(s);
     
     
-   /*
-    double xp=0.01;
-    for (double r=0.01; r<=10; r*=1.1)
-    {
-        double q1[2]; double q2[2]; double q3[2];
-        q1[0]=q2[0]=q3[0]=0;
-        q1[1]=-r; q2[0]=r; q3[0]=r;
-        std::complex<double> baryon = glasma.BaryonOperator(xp,q1,q2,q3);
-        cout << r << " " << baryon.real() << " " << baryon.imag() << endl;
-    }
-    */
     return 0;
 
 
