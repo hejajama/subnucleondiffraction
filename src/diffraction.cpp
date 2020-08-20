b7ca20330941801b66c27f8714a87cbed6f2c0ef
Heikki Mäntysaari <heikki.mantysaari@jyu.fi>
2020-08-20 09:01:15 +0300

    Remove imaginary part, faster numerics, we know it has a negligible effect

--- src/diffraction.cpp
+++ src/diffraction.cpp
@@ -269,7 +269,7 @@ double Diffraction::ScatteringAmplitudeIntegrand(double xpom, double Qsqr, doubl
     double x2[2] = {qbarx, qbary};
     double amp_real = dipole->Amplitude(xpom, x1, x2 );
     //double amp_imag = 0; // todo: check imaginary part, should vanihs... dTipole->AmplitudeImaginaryPart(xpom, x1, x2);
-    double amp_imag = dipole->AmplitudeImaginaryPart(xpom, x1, x2);
+    double amp_imag = 0; //dipole->AmplitudeImaginaryPart(xpom, x1, x2);
     std::complex<double> amp(amp_real, amp_imag);
     //amp = amp.real();   // Disable possible imag part for now
     //
