866a505f2539504b215eaf3064ecd7e676786547
Wenbin <1501110084@pku.edu.cn>
2023-04-25 08:41:29 -0400

    don't output comment

--- src/diffraction.cpp
+++ src/diffraction.cpp
@@ -181,13 +181,13 @@ double Diffraction::ScatteringAmplitude(double xpom, double Qsqr, double t, Pola
     {
         gsl_monte_vegas_state *s = gsl_monte_vegas_alloc(F.dim);
         gsl_monte_vegas_integrate(&F, lower, upper, F.dim, MCINTPOINTS/50, global_rng, s, &result, &error);
-        cout << "# vegas warmup " << result << " +/- " << error << endl;
+        //cout << "# vegas warmup " << result << " +/- " << error << endl;
         int iter=0;
         do
         {
             iter++;
             gsl_monte_vegas_integrate(&F, lower, upper, F.dim, MCINTPOINTS/5, global_rng, s, &result, &error);
-            cout << "# Vegas interation " << result << " +/- " << error << " chisqr " << gsl_monte_vegas_chisq(s) << endl;
+            //cout << "# Vegas interation " << result << " +/- " << error << " chisqr " << gsl_monte_vegas_chisq(s) << endl;
         } while (iter < 2 or fabs( gsl_monte_vegas_chisq(s) - 1.0) > 0.5);
         gsl_monte_vegas_free(s);
     }
