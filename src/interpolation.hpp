d7e78345b193f2da981c4d8b0479c36c1fadbec8
Heikki Mäntysaari <heikki.mantysaari@jyu.fi>
2025-04-23 09:57:25 +0300

    Avoid (most) memory management in the Interpolator class

--- src/interpolation.hpp
+++ src/interpolation.hpp
@@ -96,8 +96,8 @@ class Interpolator
          */
         double MaxX();
 
-        double* GetXData();
-        double* GetYData();
+        std::vector<double> GetXData();
+        std::vector<double> GetYData();
         gsl_spline* GetGslSpline() const;
         int GetNumOfPoints() const;
         INTERPOLATION_METHOD GetMethod() const;
@@ -143,8 +143,7 @@ class Interpolator
 
     private:
         INTERPOLATION_METHOD method;
-        double* xdata, *ydata;
-        bool allocated_data;    // true if we allocated xdata,ydata
+        std::vector<double> xdata, ydata;
         double minx,maxx;
         int points;
         bool ready;
